2c25b70e941bfbfaa71066b4e3ea39d717b1a715
Ayesha <aqaisar3@uwo.ca>
2023-11-21 18:47:51 -0500

    f: adding search component

--- herohub/app/page.js
+++ herohub/app/page.js
@@ -1,4 +1,5 @@
 "use client"
+import { Input, Stack, Select, Grid, GridItem } from '@chakra-ui/react'
 
 import Image from 'next/image'
 import Link from "next/link";
@@ -12,10 +13,20 @@ export default function Home() {
   <div className='container mx-auto px-4'>
     <h1>Hello World</h1>
     <p>Page content</p>
-    <div className='w-full h-screen bg-grey--300'></div>
-    <p>
-      dummry text
-    </p>
+    <div className='w-full h-40 bg-grey--800'></div>
+    <Grid templateColumns='repeat(2)' gap={10}>
+      <div className='flex justify-center items-center w-1/2'>
+        {/* need to center items */}
+        <input className="w-4/5 h-10 border border-gray-400 rounded-md focus:border-blue-50 focus:ring focus:ring-blue-300 focus:shadow-md transition duration-100 mx-5" variant='outline' placeholder='Search' />
+        <select className=" border border-gray-300 h-9 rounded-md w-1/5 max-w-screen-sm flex-1  text-blue-800 bg-gray-200 px-2" variant="filled" placeholder="Search by">
+          <option value='name'>Name</option>
+          <option value='race'>Race</option>
+          <option value='publisher'>Publisher</option>
+          <option value='Power'>Power</option>
+        </select>
+
+      </div>
+    </Grid>
 
   </div>
   </>
