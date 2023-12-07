92c0f2a4befc7cf57be1a7cc7514c191a437785f
Ayesha <aqaisar3@uwo.ca>
2023-12-06 23:17:29 -0500

    sidebar

--- client/herohub/app/components/navigation/sidebar/page.js
+++ client/herohub/app/components/navigation/sidebar/page.js
@@ -1,9 +1,48 @@
 import Link from "next/link"
+import React, {useState, useEffect} from 'react'
 
 const Sidebar = ({
     isOpen,
     toggle,
 }) => {
+    const [user, setUser] = useState(null)
+    const backPort ='3001'
+
+    useEffect(()=>{
+        (
+          async()=>{
+            const response = await fetch(`http://localhost:${backPort}/api/auth`, {
+              credentials:'include'
+            });
+            const content = await response.json()
+            setUser(content)
+            //console.log(content)
+          } 
+        )()})
+
+        const handleLogout = async()=>{
+            try{
+             
+              const response = await fetch(`http://localhost:${backPort}/logout`, {
+                credentials:'include'
+              })
+              console.log(response)
+      
+              if(response.ok){
+                //const pastDate = new Date(0)
+                //document.cookie = `${name}=; expires=${pastDate.toUTCString()}; path=/login;`
+                alert("You are logged out. Redirecting...")
+                window.location.href='/'
+              }else{
+                console.error('Logout failed')
+              }
+            }catch(err){
+              console.log("error lgout", err)
+            }
+      
+        
+          }
+
     return (
         <>
         <div className="sidebar-container fixed w-full h-full overflow-hidden justify-center bg-white grid pt-[120px] left-0 z-10"
@@ -22,12 +61,28 @@ const Sidebar = ({
             </button>
 
             <ul className="sidebar-nav text-center leading-relaxed text-xl">
-          <li>
-            <Link href="/register" onClick={toggle}><p>Register</p></Link>
-          </li>
-          <li>
-            <Link href="/sign-in" onClick={toggle}><p>Sign In</p></Link>
-          </li>
+            
+            <li>
+                  {user &&(
+                    <Link href={user.account === 'admin'? "/admin": "/user-profile"}>
+                      <p>Profile</p>
+                </Link>)}
+                    
+                </li>
+                <li>
+                        {user? <p onClick={handleLogout} className='cursor-pointer'>Logout</p>:'' }
+                </li>
+
+            <li>
+                <Link href="/register">
+                        {user? '': <p>Register</p>}
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/login">
+                    {user? '': <p>Sign-in</p>}
+                    </Link>
+                </li>
         </ul>
         
         </div>
